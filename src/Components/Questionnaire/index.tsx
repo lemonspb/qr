ea018bebe5d2b3e4f610126e0ca14d527fa0d7bf
Misha <misha.skopenko@yandex.ru>
2020-09-09 16:57:35 +0300

    add blank styles

--- src/Components/Questionnaire/index.tsx
+++ src/Components/Questionnaire/index.tsx
@@ -1,87 +1,39 @@
 
 import React, { useEffect } from "react";
-import './style.scss'
-import {Blank}  from '../../Interfaces'
-import * as Survey from "survey-react"
-import "survey-react/survey.css"
-import {mockupFirst} from '../../Mockups'
+import './style.scss';
+import {Blank}  from '../../Interfaces';
+import * as Survey from "survey-react";
+import "survey-react/survey.css";
 interface Questionnaire {
-    blank: Blank,
+    data: any,
 }
 
 const Questionnaire = (props: Questionnaire) => {
 
-    const { blank } = props
+    const { data } = props
 
-  
+    var myCss = {
+        navigation:{
+            "complete": 'button'
+        }
+    };
+
+    const surveyValueChanged = function (sender:any, options:any) {
+        console.log(sender,options)
+        var el = document.getElementById(options.name);
+        if (el) {
+            console.log(el,'++++++++++++++')
+        }
+    };
+    
 
     return (
-        
-        <form className='questionnaire'>
-   <Survey.Survey
-        json={{
-            showQuestionNumbers: "off",
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "haveKids",
-                    title: "Do you have a kid(s)?",
-                    isRequired: true,
-                    choices: [
-                        "Yes", "No"
-                    ],
-                    colCount: 0
-                }, {
-                    type: "dropdown",
-                    name: "kids",
-                    title: "How many kids do you have",
-                    visibleIf: "{haveKids}='Yes'",
-                    isRequired: true,
-                    choices: [1, 2, 3, 4, 5]
-                }, {
-                    type: "dropdown",
-                    name: "kid1Age",
-                    title: "The first kid age:",
-                    visibleIf: "{haveKids}='Yes' and {kids} >= 1",
-                    isRequired: true,
-                    "choicesMax": 18
-                }, {
-                    type: "dropdown",
-                    name: "kid2Age",
-                    title: "The second kid age:",
-                    visibleIf: "{haveKids}='Yes' and {kids} >= 2",
-                    isRequired: true,
-                    startWithNewLine: false,
-                    "choicesMax": 18
-                }, {
-                    type: "dropdown",
-                    name: "kid3Age",
-                    title: "The third kid age:",
-                    visibleIf: "{haveKids}='Yes' and {kids} >= 3",
-                    isRequired: true,
-                    startWithNewLine: false,
-                    "choicesMax": 18
-                }, {
-                    type: "dropdown",
-                    name: "kid4Age",
-                    title: "The fourth kid age:",
-                    visibleIf: "{haveKids}='Yes' and {kids} >= 4",
-                    isRequired: true,
-                    startWithNewLine: false,
-                    "choicesMax": 18
-                }, {
-                    type: "dropdown",
-                    name: "kid5Age",
-                    title: "The fifth kid age:",
-                    visibleIf: "{haveKids}='Yes' and {kids} >= 5",
-                    isRequired: true,
-                    startWithNewLine: false,
-                    "choicesMax": 18
-                }
-            ]
-        }}
-      />
-        </form>
+        <div className='questionnaire'>
+        <Survey.Survey json={data}
+        css={myCss}
+        onValueChanged={surveyValueChanged}
+        />
+        </div>
     )
 
 }
--- src/Pages/PageQuestions/index.tsx
+++ src/Pages/PageQuestions/index.tsx
@@ -1,31 +1,18 @@
 import React from 'react';
 import Title from '../../Components/Title'
-import * as Survey from "survey-react"
-import "survey-react/survey.css"
+import Questionnaire from '../../Components/Questionnaire'
 import {mockupFirst} from '../../Mockups'
+
 import './style.scss'
 function PageQuestions() {
-  var mycustomSurveyStrings = {
-    pagePrevText: "My Page Prev",
-    pageNextText: "My Page Next",
-    completeText: "OK - Press to Complete"
-};
-Survey
-    .surveyLocalization
-    .locales["ru"] = mycustomSurveyStrings;
+
+
 
   return (
     <div className='page-questions'>
-      <Title 
-      Tag='h2'
-      text='Опрос удовлетворенности пациента условиями оказания медицинской помощи'
-      position='center'
-      />
       <div className='page-questions__wrap'>
 
-         <Survey.Survey
-        json={mockupFirst}
-      />
+        <Questionnaire  data={mockupFirst} />
         </div>
     </div>
 
